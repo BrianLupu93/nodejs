119779263567614cc10ede8afd85c14bd367450a
BrianLupu93 <brianlupu18@gmail.com>
2022-12-11 16:44:23 +0100

    read, write, update and rename file async

--- 02/index.js
+++ 02/index.js
@@ -1,23 +1,65 @@
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 const path = require("path");
 
-fs.readFile(path.join(__dirname, "starter.txt"), "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "starter.txt"),
+      "utf8"
+    );
 
-fs.writeFile(path.join(__dirname, "reply.txt"), "Nice to meet you", (err) => {
-  if (err) throw err;
-  console.log("Write complete");
-});
+    console.log(data);
 
-fs.appendFile(path.join(__dirname, "test.txt"), "Brian test", (err) => {
-  if (err) throw err;
-  console.log("Append complete");
-});
+    await fsPromises.unlink(path.join(__dirname, "starter.txt"));
+
+    await fsPromises.writeFile(path.join(__dirname, "promiseWrite.txt"), data);
+    await fsPromises.appendFile(
+      path.join(__dirname, "promiseWrite.txt"),
+      "This is what i append assynchrously!"
+    );
+    await fsPromises.rename(
+      path.join(__dirname, "promiseWrite.txt"),
+      path.join(__dirname, "promiseRename.txt")
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fileOps();
+
+// fs.readFile(path.join(__dirname, "starter.txt"), "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.writeFile(path.join(__dirname, "reply.txt"), "Nice to meet you", (err) => {
+//   if (err) throw err;
+//   console.log("Write complete");
+
+//   fs.appendFile(
+//     path.join(__dirname, "reply.txt"),
+//     "\n\n Hello there test",
+//     (err) => {
+//       if (err) throw err;
+//       console.log("Append complete");
+
+//       fs.rename(
+//         path.join(__dirname, "reply.txt"),
+//         path.join(__dirname, "newReply.txt"),
+
+//         (err) => {
+//           if (err) throw err;
+//           console.log("Rename complete");
+//         }
+//       );
+//     }
+//   );
+// });
+
 // exit on uncaught errors
 
-process.on("uncaughtException", (err) => {
-  console.error(`There was an uncaught error: ${err}`);
-  process.exit(1);
-});
+// process.on("uncaughtException", (err) => {
+//   console.error(`There was an uncaught error: ${err}`);
+//   process.exit(1);
+// });
---
+++ 02/stream.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const rs = fs.createReadStream("lorem.txt", {
+  encoding: "utf8",
+});
+
+const ws = fs.createWriteStream("lorem.txt");
+
+// rs.on("data", (dataChunk) => {
+//   ws.write(dataChunk);
+// });
+
+rs.pipe(ws);
